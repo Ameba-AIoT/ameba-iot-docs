fa141b494c14dc8533582a7289630fb7e9de9227
terra_cai <terra_cai@realsil.com.cn>
2025-01-10 14:31:29 +0800

    [wifi][bridge] add diagram pic for bridge solution * add diagram pic for bridge solution

---
+++ ameba/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "build amebadplus"
+cd ../amebadplus
+make en
+
+counter=5
+until [ $counter -eq 0 ]
+do
+    sleep 1
+    echo "build ameba common after "$counter" seconds"
+    ((counter--))
+done
+
+cd -
+make en
