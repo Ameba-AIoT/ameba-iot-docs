96fa4af73fa5cb7d89ec52a04ca7f883d0287296
terra_cai <terra_cai@realsil.com.cn>
2025-02-11 15:02:26 +0800

    nda optimize

--- amebadplus/build.sh
+++ amebadplus/build.sh
@@ -2,6 +2,10 @@
 
 if [$1 -eq "cn"]; then
     make cn TAGS=8721D
+elif [$1 -eq "cnda"]; then
+    make cn TAGS="nda 8721D"
+elif [$1 -eq "enda"]; then
+    make en TAGS="nda 8721D"
 else
     make en TAGS=8721D
 fi
