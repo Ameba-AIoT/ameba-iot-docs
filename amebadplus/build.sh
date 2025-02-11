a3e3415055d3944c4ecf7be8a35216ea9869c517
terra_cai <terra_cai@realsil.com.cn>
2025-02-11 17:01:50 +0800

    fix index wrong issue

--- amebadplus/build.sh
+++ amebadplus/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-if [[$1 == "cn"]]; then
-    make cn TAGS=8721D
-elif [[$1 == "cnda"]]; then
-    make cn TAGS="nda 8721D"
-elif [[$1 == "enda"]]; then
-    make en TAGS="nda 8721D"
-else
-    make en TAGS=8721D
-fi
+# 获取输入的字符串
+input_string="$1"
+
+# 根据字符串内容执行不同的操作
+case "$input_string" in
+    "cn")
+        echo "build cn ..."
+        make cn TAGS="8721D"
+        ;;
+    "cnda")
+        echo "build cn nda ..."
+        make cn TAGS="nda 8721D"
+        ;;
+    "enda")
+        echo "build en nda ..."
+        make en TAGS="nda 8721D"
+        ;;
+    *)
+        make en TAGS="8721D"
+        ;;
+esac
